069bfebde86c675f467b18766b95885cdfb655f5
perezchris3599 <kweeziperez712@gmail.com>
2022-07-04 14:40:23 +0300

    first commit

---
+++ practice.py
@@ -0,0 +1,20 @@
+#functions
+#def function3(x, y):
+#    return x + y
+#e = function3(1, 2)
+#print(e)
+
+#dictionaries
+d ={}
+d["George"] = 24
+d["Tom"] = 32
+d["Jenny"] = 16
+
+print(d["George"])
+
+for key, value in d.items():
+    print("key:")
+    print(key)
+    print("value:")
+    print(value)
+    print("")
