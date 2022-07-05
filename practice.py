f4da896157622448551472d54a70bcb203501bbc
perezchris3599 <kweeziperez712@gmail.com>
2022-07-05 15:50:10 +0300

    using range

--- practice.py
+++ practice.py
@@ -153,3 +153,11 @@ total2 = 0
 for i in range(1, 5):
     total2 += i
 print(total2)
+#using range
+print(list(range(1, 8)))
+#getting total of the range
+total3 = 0
+for i in range(1, 8):
+    if i % 3 == 0:
+        total3 += i
+print(total3)
