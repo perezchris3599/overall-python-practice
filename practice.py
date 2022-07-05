c5c6477e2a22c3f10b57b379615abb51238ebefe
perezchris3599 <kweeziperez712@gmail.com>
2022-07-05 18:22:02 +0300

    sum of positive numbers

--- practice.py
+++ practice.py
@@ -1,8 +1,8 @@
 #functions
-#def function3(x, y):
-#    return x + y
-#e = function3(1, 2)
-#print(e)
+def function3(x, y):
+    return x + y
+e = function3(1, 2)
+print(e)
 
 #dictionaries
 d ={}
@@ -205,4 +205,14 @@ while j < 5:
     total2 += j
     j += 1
 print(total2)
+
+#finding the sum of only positive numbers in a list using the while loop
+given_list = [5, 4, 4, 3, 1, -2, -3, -5]
+
+total3 = 0
+i = 0
+while given_list[i] > 0:
+    total3 += given_list[i]
+    i += 1
+print(total3)
 
