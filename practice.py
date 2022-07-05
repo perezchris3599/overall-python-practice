1eff156da7eb5437692f63adb65378107d4527eb
perezchris3599 <kweeziperez712@gmail.com>
2022-07-05 18:08:37 +0300

    while loop and break

--- practice.py
+++ practice.py
@@ -197,3 +197,12 @@ while given_list[j] < 0:
     total2 += given_list[j]
     j -= 1
 print(total2)
+
+#while loops and the break statement
+total2 = 0
+j = 1
+while j < 5:
+    total2 += j
+    j += 1
+print(total2)
+
