9cbcc7a4db707c478001919542caa5a39c246ae8
perezchris3599 <kweeziperez712@gmail.com>
2022-07-06 16:07:54 +0300

    created object 1

--- oop.py
+++ oop.py
@@ -16,3 +16,10 @@
 class Robot:
     def introduce_self(self):
         print("My name is " + self.name)
+
+#create an object
+r1 = Robot()
+r1.name = "Tom"
+r1.color = "red"
+r1.weight = "30"
+r1.introduce_self()
