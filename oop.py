dd8a9113d80d313672d24da9870a77feab11468e
perezchris3599 <kweeziperez712@gmail.com>
2022-07-06 17:04:28 +0300

    new operator and object

--- oop.py
+++ oop.py
@@ -14,22 +14,32 @@
 
 #defining the class
 class Robot:
+#adding constructor
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+
     def introduce_self(self):
         print("My name is " + self.name)
         print("My color is " + self.color)
 
 #create an object
-r1 = Robot()
-r1.name = "Tom"
-r1.color = "red"
-r1.weight = 30
+#r1 = Robot()
+#r1.name = "Tom"
+#r1.color = "red"
+#r1.weight = 30
 
 #created object 2
-r2 = Robot()
-r2.name = "Jerry"
-r2.color = "blue"
-r2.weight = 40
+#r2 = Robot()
+#r2.name = "Jerry"
+#r2.color = "blue"
+#r2.weight = 40
 
+#no need to specify attribute names
+#both object one and two summarized
+r1 = Robot("Tom", "red", 30)
+r2 = Robot("Jerry", "blue", 40)
 
 r1.introduce_self()
 r2.introduce_self()
