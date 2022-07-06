da4974934922374aae415b1afaf763e80690e94b
perezchris3599 <kweeziperez712@gmail.com>
2022-07-06 20:55:35 +0300

    removed class

--- oop.py
+++ oop.py
@@ -1,16 +1,3 @@
-#classes and objects in oop
-#class Robot {
-#    String name;
-#    String color;
-#    int weight;
-
-    #adding constructor/function
-#    Robot(String n, String c, int w) {
-#        this.name = n;
-#        this.color = c;
-#        this.weight = w;
-#    }
-#}
 
 #defining the class
 class Robot:
