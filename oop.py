238216ce650d5d0ef101cfc4a683bcb4ef16a7ea
perezchris3599 <kweeziperez712@gmail.com>
2022-07-06 15:30:30 +0300

    intro to classes and objects

---
+++ oop.py
@@ -0,0 +1,13 @@
+#classes and objects in oop
+class Robot {
+    String name;
+    String color;
+    int weight;
+
+    #adding constructor/function
+    Robot(String n, String c, int w) {
+        this.name = n;
+        this.color = c;
+        this.weight = w;
+    }
+}
