e40c6f07d2cc1ffdb972c3e60c6ae769c2222253
perezchris3599 <kweeziperez712@gmail.com>
2022-07-06 20:46:38 +0300

    object and classes interaction

--- oop.py
+++ oop.py
@@ -43,3 +43,21 @@ r2 = Robot("Jerry", "blue", 40)
 
 r1.introduce_self()
 r2.introduce_self()
+
+#how multiple objects and classes interact with each other
+class Person:
+    def __init__(self, n, p, i):
+        self.name = n
+        self.personality = p
+        self.is_sitting = i
+    
+    def sit_down(self):
+        self.is_sitting = True
+
+    def stand_up(self):
+        self.is_sitting = False
+
+p1 = Person("Alice", "aggressive", False)
+p2 = Person("Becky", "talkative", True)
+
+
