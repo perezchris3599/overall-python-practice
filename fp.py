83549bdbad1de089ecbe691260a3c9b769d435cc
perezchris3599 <kweeziperez712@gmail.com>
2022-07-18 16:30:00 +0300

    add functions to dictionaries

--- fp.py
+++ fp.py
@@ -2,3 +2,14 @@
 #pure functions
 def do_Pure(data):
     return data * 2 #this returns the data multiplied by two
+
+#functions are objects
+def func1():
+    return 1
+def func2():
+    return 2
+
+#adding functions to dictionaries
+my_funcs = {'a': func1, 'b':func2}
+my_funcs['a']()
+my_funcs['b']()
