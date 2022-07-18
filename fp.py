af2e495632f2ef641c40729f63c939e2af25c2e0
perezchris3599 <kweeziperez712@gmail.com>
2022-07-18 17:25:00 +0300

    oeom

--- fp.py
+++ fp.py
@@ -81,3 +81,16 @@ function, f()
 
 # map does
 list, [f(m), f(n), f(p)]
+
+#other map examples
+def square (lst1):
+    lst2 = []
+    for num in lst1:
+        lst2.append(num ** 2)
+    return lst2
+
+print square([4,3,2,1])
+
+#map
+n = [4, 3, 2, 1]
+print (list(map(lambda x: x**2, n)))
