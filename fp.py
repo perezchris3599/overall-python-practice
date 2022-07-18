0b8ecbea90ce80f873a4b4ef909fed46ac860ad0
perezchris3599 <kweeziperez712@gmail.com>
2022-07-18 16:33:34 +0300

    closures and currying

--- fp.py
+++ fp.py
@@ -13,3 +13,17 @@ def func2():
 my_funcs = {'a': func1, 'b':func2}
 my_funcs['a']()
 my_funcs['b']()
+
+#closures and currying
+def outer(outer_arg):
+    def inner(inner_arg):
+        return inner_arg + outer_arg
+    return inner
+
+func = outer(10)
+func(5)
+
+func.__closure__
+func.__closure__[0]
+func.__closure__[0].cell_contents
+
